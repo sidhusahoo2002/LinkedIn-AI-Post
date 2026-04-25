0bccd74c1caa70eb32f9ef4821e63aa69841e279
siddheswar <impeccablesidhu2002@gmail.com>
2026-04-26 00:38:45 +0530

    cors origin updated

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import { GoogleGenAI } from "@google/genai";
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 
 let latestApprovedPost = "";
 let postsDB = [];
